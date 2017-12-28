60439a7b44a0189688fd556cc29dc25f2f91a9ee
yuiseshim <stochasticleastactionprinciple@gmail.com>
2017-12-29 08:58:00 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -2,12 +2,13 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew cask install emacs
-#brew cask install google-chrome
-brew cask install google-drive
+brew cask install google-chrome
+#brew cask install google-drive
 brew cask install xquartz
 brew cask install github
 brew cask install texshop
 brew cask install aquaskk
+brew cask install kindle
 brew cask cleanup
 
 brew install ghostscript
