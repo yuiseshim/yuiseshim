88f1c83131d7fd81043e682db242dd25971d1043
yuiseshim <stochasticleastactionprinciple@gmail.com>
2018-01-01 10:58:02 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -22,6 +22,7 @@ brew install hdf5
 brew install mecab mecab-jumandic mecab-ko-dic mecab-unidic-extended mecab-ipadic mecab-ko mecab-unidic
 brew install pandoc
 brew install graphviz
+brew install node
 brew cleanup
 
 
