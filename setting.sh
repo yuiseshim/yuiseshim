d7bcb9f1e3ecbf3af37bea8c13748c70b2b8654d
yuiseshim <stochasticleastactionprinciple@gmail.com>
2018-01-17 21:36:39 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -12,6 +12,7 @@ brew cask install java
 brew cask install lilypond
 brew cask install google-backup-and-sync
 brew cask install mactex
+brew cask install libreoffice
 
 brew tap caskroom/drivers
 brew cask install kensington-trackball-works
