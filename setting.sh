cb3b6d803ca5ff31c6d4d94707973b363e87a8d6
yuiseshim <stochasticleastactionprinciple@gmail.com>
2018-03-06 08:13:49 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -65,3 +65,7 @@ cd ~/Downloads
 git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
 cd mecab-ipadic-neologd
 ./bin/install-mecab-ipadic-neologd -n
+
+# apple app update
+# brew tap buo/cask-upgrade
+# brew cu
