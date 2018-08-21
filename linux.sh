e9baa9b7a4c471cf4708e401785a77c3806af6b1
yuiseshim <stochasticleastactionprinciple@gmail.com>
2018-08-21 11:32:12 +0900

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -3,4 +3,4 @@ sudo apt-get -y install build-essential curl git python-setuptools ruby file
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 
 # 
-export PATH='/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin':"$PATH"
+echo "PATH='/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin':$PATH" >>~/.profile
