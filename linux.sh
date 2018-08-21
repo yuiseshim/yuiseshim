3a8a1f68d622b15b97f79c1ffb1ba9883793aa5d
yuiseshim <stochasticleastactionprinciple@gmail.com>
2018-08-21 11:29:29 +0900

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,5 +1,6 @@
-sudo apt-get -y install build-essential curl git python-setuptools ruby
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/linuxbrew/go/install)"
-export PATH="$HOME/.linuxbrew/bin:$PATH"
-export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
-export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+# http://linuxbrew.sh/
+sudo apt-get -y install build-essential curl git python-setuptools ruby file
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+# 
+export PATH='/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin':"$PATH"
