17b338661720ba543c34df9df736c9e187005fad
yuiseshim <stochasticleastactionprinciple@gmail.com>
2018-08-21 20:05:30 +0900

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -7,3 +7,8 @@ echo 'PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin":"$PA
 
 sudo apt-get -y install gnome-core
 sudo apt-get -y install gnome-panel
+
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install google-chrome-stable
