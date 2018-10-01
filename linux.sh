ce80308dde4473f3fca1865b38025ee579e7a675
yuiseshim <stochasticleastactionprinciple@gmail.com>
2018-10-01 09:53:58 +0900

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -22,3 +22,13 @@ sudo update-alternatives --config x-session-manager
 # English description
 LANG=C xdg-user-dirs-gtk-update
 #xdg-user-dirs-gtk-update
+
+###### bazel #########
+apt-get update
+apt-get install -y openjdk-8-jdk
+
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+
+apt-get update && apt-get install -y bazel
+apt-get upgrade bazel
