93334f775732580e6b1e7431d636143c64c7663b
yuiseshim <stochasticleastactionprinciple@gmail.com>
2019-03-21 00:02:10 +0900

    Update jupyterlab.sh

--- jupyterlab.sh
+++ jupyterlab.sh
@@ -1,1 +1,6 @@
 jupyter labextension install @mflevine/jupyterlab_html
+jupyter labextension install @jupyterlab/plotly-extension
+jupyter labextension install @jupyterlab/latex
+jupyter labextension install @jupyterlab/git
+jupyter labextension install @jupyterlab/toc
+jupyter labextension install jupyterlab_vim
