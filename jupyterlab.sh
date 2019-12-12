d0aec284fc1b826e21743f4a2016e939c8ebb3c0
yuiseshim <stochasticleastactionprinciple@gmail.com>
2019-12-13 00:21:56 +0900

    Update jupyterlab.sh

--- jupyterlab.sh
+++ jupyterlab.sh
@@ -2,6 +2,8 @@ jupyter labextension install @mflevine/jupyterlab_html
 jupyter labextension install @jupyterlab/plotly-extension
 jupyter labextension install @jupyterlab/latex
 jupyter labextension install @jupyterlab/git
+pip install jupyterlab-git
+jupyter serverextension enable --py jupyterlab_git
 jupyter labextension install @jupyterlab/toc
 jupyter labextension install @lckr/jupyterlab_variableinspector
 jupyter labextension install dcos-jupyterlab-tensorboard-extension
@@ -9,6 +11,10 @@ jupyter labextension install jupyterlab-spreadsheet
 jupyter labextension install @jupyterlab/google-drive
 jupyter serverextension enable jupyterlab_sql --py --sys-prefix
 jupyter labextension install @lckr/jupyterlab_variableinspector
+pip install ipywidgets
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+jupyter nbextension enable --py --sys-prefix widgetsnbextension
 #jupyter labextension install jupyterlab_tensorboard
+#jupyter serverextension enable --py jupyterlab_tensorboard
 #jupyter labextension install jupyterlab_vim
 jupyter lab build
