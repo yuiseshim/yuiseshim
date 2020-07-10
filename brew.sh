209942c119e24d608ef834b6cc221344b858a367
yuiseshim <stochasticleastactionprinciple@gmail.com>
2020-07-10 16:04:57 +0900

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -18,7 +18,6 @@ brew cask install libreoffice
 brew cask install the-unarchiver
 brew cask install slack
 brew cask install karabiner-elements
-brew cask install chromedriver
 brew cask install google-cloud-sdk
 brew cask install tunnelblick
 brew cask install gimp
