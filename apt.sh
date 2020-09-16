00e02c30045ac66f05f9dc168552f3d11090837f
yuiseshim <stochasticleastactionprinciple@gmail.com>
2020-09-16 15:00:10 +0900

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -4,13 +4,13 @@ sudo apt-get -y emacs
 sudo apt-get install -y openssh-server
 sudo apt-get install -y swig
 
+# ExpanDrive
 exfs --help 
 #https://www.expandrive.com/expandrive-for-linux/
 #sudo apt install ./ExpanDrive_7.5.0_amd64.deb
 curl https://packages.expandrive.com/keys/expandrive.asc | gpg --dearmor > packages.expandrive.gpg
 sudo install -o root -g root -m 644 packages.expandrive.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.expandrive.gpg] https://packages.expandrive.com/expandrive stable main" > /etc/apt/sources.list.d/expandrive.list'
-
 sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install -y expandrive
@@ -19,7 +19,6 @@ apt-get install tetex-*
 apt-get install texlive
 apt-get install texlive-lang-cjk
 apt-get install xdvik-ja
-sudo apt-get install mecab libmecab-dev mecab-ipadic mecab-ipadic-utf8
 sudo apt-get install wkhtmltopdf #html2image
 
 # Install pyenv-virtual
