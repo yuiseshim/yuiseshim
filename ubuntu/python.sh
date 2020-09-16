525f57a54402ce3cda0dbb76b514ec65546f913b
yuiseshim <stochasticleastactionprinciple@gmail.com>
2020-09-16 15:31:59 +0900

    Update python.sh

--- ubuntu/python.sh
+++ ubuntu/python.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 
-echo -e "\n# `date '+%Y/%m/%d'` H.Seshime" >> ~/.profile
+echo -e "\n# `date '+%Y/%m/%d'` $USER" >> ~/.profile
 echo "## Setting pyenv, pyenv-virtualenv ##" >> ~/.profile
 echo 'export PATH="$PATH:$HOME/.pyenv/shims"' >> ~/.profile
 echo 'export PYENV_ROOT="${HOME}/.pyenv"' >> ~/.profile
@@ -10,7 +10,7 @@ echo 'if [ -d "${PYENV_ROOT}" ]; then' >> ~/.profile
 echo '\t export PATH=${PYENV_ROOT}/bin:$PATH'
 echo '\t eval "$(pyenv init -)"' >> ~/.profile
 echo '\t eval "$(pyenv virtualenv-init -)"' >> ~/.profile
-echo 'fi'
+echo 'fi\n'
 echo 'export PYTHONPATH="$HOME/Desktop"' >> ~/.profile
 source ~/.bashrc
 
