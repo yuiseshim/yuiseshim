0fb1ce8b9d026f197835f9b6868ea7b6d995c322
yuiseshim <stochasticleastactionprinciple@gmail.com>
2020-09-16 10:27:36 +0900

    Create apt.sh

---
+++ apt.sh
@@ -0,0 +1,10 @@
+#https://www.expandrive.com/expandrive-for-linux/
+#sudo apt install ./ExpanDrive_7.5.0_amd64.deb
+
+curl https://packages.expandrive.com/keys/expandrive.asc | gpg --dearmor > packages.expandrive.gpg
+sudo install -o root -g root -m 644 packages.expandrive.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.expandrive.gpg] https://packages.expandrive.com/expandrive stable main" > /etc/apt/sources.list.d/expandrive.list'
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install expandrive
