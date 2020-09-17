c41237c38d7405aecb67d50375d7a5ea966019d3
yuiseshim <stochasticleastactionprinciple@gmail.com>
2020-09-18 04:41:48 +0900

    Update apt.sh

--- ubuntu/apt.sh
+++ ubuntu/apt.sh
@@ -43,7 +43,12 @@ git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
 cd mecab-ipadic-neologd
 ./bin/install-mecab-ipadic-neologd -n
 
-# Google Drive
+# Google Chrome & Drive
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
 sudo add-apt-repository ppa:alessandro-strada/ppa
 #sudo add-apt-repository ppa:alessandro-strada/google-drive-ocamlfuse-beta
 sudo apt-get update
