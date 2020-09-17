9525c2595367db7fea5fd5f004480b63733165b0
h_seshime <h_seshime@62-35649.local>
2020-09-18 07:20:46 +0900

    update

---
+++ ubuntu/profile.sh
@@ -0,0 +1,15 @@
+echo '# ALIAS' >> .profile
+echo 'if [ -f "$HOME/.bash_alias" ]; then' >> .profile
+echo '  . "$HOME/.bash_alias"' >> .profile
+echo 'fi' >> .profile
+
+echo 'export PYTHONPATH="$HOME/Desktop"' >> .profile
+
+echo '# API KEY' >> .profile
+echo 'if [ -f ~/Desktop/.bash_mykey ] ; then' >> .profile
+echo '  . ~/Desktop/.bash_mykey' >> .profile
+echo 'fi' >> .profile
+
+echo '# GCP' >> .profile
+echo 'export GOOGLE_APPLICATION_CREDENTIALS="***"' >> .profile
+
