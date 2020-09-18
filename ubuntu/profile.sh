f134250731ede05c6135b9cd81f8237ca92cb130
yuiseshim <stochasticleastactionprinciple@gmail.com>
2020-09-18 11:14:47 +0900

    Update profile.sh

--- ubuntu/profile.sh
+++ ubuntu/profile.sh
@@ -8,7 +8,7 @@ echo 'fi' >> ~/.profile
 echo -e '\n'
 
 echo '# API KEY' >> ~/.profile
-echo 'if [ -f ~/.bash_mykey ] ; then' >> ~/.profile
-echo '  . ~/.bash_mykey' >> ~/.profile
+echo 'if [ -f "$HOME/.bash_mykey" ] ; then' >> ~/.profile
+echo '  . "$HOME/.bash_mykey"' >> ~/.profile
 echo 'fi' >> ~/.profile
 echo -e '\n'
