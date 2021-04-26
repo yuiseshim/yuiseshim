8fa95ea06f89c4360c65debac11c4e712b8987ab
yuiseshim <stochasticleastactionprinciple@gmail.com>
2021-04-26 02:13:42 -0700

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,42 +1,75 @@
 #HomeBrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$HOME/.zprofile
+#brew cask install emacs
+#brew cask install google-chrome
+#brew cask install chromedriver
+#brew cask install xquartz
+#brew cask install github
+#brew cask install texshop
+#brew cask install aquaskk
+#brew cask install kindle
+#brew cask install java
+#brew cask install lilypond
+#brew cask install google-backup-and-sync
+#brew cask install firefox --language=ja
+#brew cask install mactex
+#brew cask install libreoffice
+#brew cask install the-unarchiver
+#brew cask install slack
+#brew cask install karabiner-elements
+#brew cask install google-cloud-sdk
+#brew cask install tunnelblick
+#brew cask install gimp
+#brew cask install firealpaca
+#brew cask install Tunnelblick
+#brew cask install garmin-express
 
-brew cask install emacs
-brew cask install google-chrome
-brew cask install chromedriver
-brew cask install xquartz
-brew cask install github
-brew cask install texshop
-brew cask install aquaskk
-brew cask install kindle
-brew cask install java
-brew cask install lilypond
-brew cask install google-backup-and-sync
-brew cask install firefox --language=ja
-brew cask install mactex
-brew cask install libreoffice
-brew cask install the-unarchiver
-brew cask install slack
-brew cask install karabiner-elements
-brew cask install google-cloud-sdk
-brew cask install tunnelblick
-brew cask install gimp
-brew cask install firealpaca
-brew cask install Tunnelblick
-brew cask install garmin-express
+#brew tap caskroom/drivers
+#brew cask install kensington-trackball-works
 
-brew tap caskroom/drivers
-brew cask install kensington-trackball-works
+#brew cask install onyx
+#brew cask install macdown
+#brew cask install microsoft-office
+#brew cask install onedrive
+#brew cask install wkhtmltopdf
+#brew cask install flash-player
+#brew cask install box-drive 
+#brew cask install evernote
+#brew cask cleanup
 
-brew cask install onyx
-brew cask install macdown
-brew cask install microsoft-office
-brew cask install onedrive
-brew cask install wkhtmltopdf
-brew cask install flash-player
-brew cask install box-drive 
-brew cask install evernote
-brew cask cleanup
+brew install emacs
+brew install google-chrome
+brew install chromedriver
+brew install xquartz
+brew install github
+brew install texshop
+brew install aquaskk
+brew install kindle
+brew install java
+brew install lilypond
+brew install google-backup-and-sync
+brew install firefox --language=ja
+brew install mactex
+brew install libreoffice
+brew install the-unarchiver
+brew install slack
+brew install karabiner-elements
+brew nstall google-cloud-sdk
+brew install tunnelblick
+brew install gimp
+brew install firealpaca
+
+brew install onyx
+brew install macdown
+brew install microsoft-office
+brew install onedrive
+brew install wkhtmltopdf
+brew install flash-player
+brew install box-drive 
+brew install evernote
+brew cleanup
 
 brew install zsh-completions zsh-autosuggestions zsh-syntax-highlighting
 brew install wget
